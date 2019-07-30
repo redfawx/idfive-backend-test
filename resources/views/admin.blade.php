2d751c9a6247d93b77b86be7ce68a49f93f3d850
Stephen Toms <steve@Stephens-MacBook-Pro.local>
2019-07-30 00:13:11 -0400

    created basic styles for admin/page 2 of assessment

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,79 +1,86 @@
 @include('layout.header')
 
-<!-- <div class="select-bar">
-    <select>
-        <option value="AUD">AUD</option>
-        <option value="BGN">BGN</option>
-        <option value="BRL">BRL</option>
-        <option value="CAD">CAD</option>
-        <option value="CHF">CHF</option>
-        <option value="CNY">CNY</option>
-        <option value="CZK">CZK</option>
-        <option value="DKK">DKK</option>
-        <option value="EUR">EUR</option>
-        <option value="GBP">GBP</option>
-        <option value="HKD">HKD</option>
-        <option value="HRK">HRK</option>
-        <option value="HUF">HUF</option>
-        <option value="IDR">IDR</option>
-        <option value="ILS">ILS</option>
-        <option value="INR">INR</option>
-        <option value="ISK">ISK</option>
-        <option value="JPY">JPY</option>
-        <option value="KRW">KRW</option>
-        <option value="MXN">MXN</option>
-        <option value="MYR">MYR</option>
-        <option value="NOK">NOK</option>
-        <option value="NZD">NZD</option>
-        <option value="PHP">PHP</option>
-        <option value="PLN">PLN</option>
-        <option value="RON">RON</option>
-        <option value="RUB">RUB</option>
-        <option value="SEK">SEK</option>
-        <option value="SGD">SGD</option>
-        <option value="THB">THB</option>
-        <option value="TRY">TRY</option>
-        <option value="USD">USD</option>
-        <option value="ZAR">ZAR</option>
-    </select>
-    <select>
-        <option value="AUD">AUD</option>
-        <option value="BGN">BGN</option>
-        <option value="BRL">BRL</option>
-        <option value="CAD">CAD</option>
-        <option value="CHF">CHF</option>
-        <option value="CNY">CNY</option>
-        <option value="CZK">CZK</option>
-        <option value="DKK">DKK</option>
-        <option value="EUR">EUR</option>
-        <option value="GBP">GBP</option>
-        <option value="HKD">HKD</option>
-        <option value="HRK">HRK</option>
-        <option value="HUF">HUF</option>
-        <option value="IDR">IDR</option>
-        <option value="ILS">ILS</option>
-        <option value="INR">INR</option>
-        <option value="ISK">ISK</option>
-        <option value="JPY">JPY</option>
-        <option value="KRW">KRW</option>
-        <option value="MXN">MXN</option>
-        <option value="MYR">MYR</option>
-        <option value="NOK">NOK</option>
-        <option value="NZD">NZD</option>
-        <option value="PHP">PHP</option>
-        <option value="PLN">PLN</option>
-        <option value="RON">RON</option>
-        <option value="RUB">RUB</option>
-        <option value="SEK">SEK</option>
-        <option value="SGD">SGD</option>
-        <option value="THB">THB</option>
-        <option value="TRY">TRY</option>
-        <option value="USD">USD</option>
-        <option value="ZAR">ZAR</option>
-    </select>
+<div class="select-bar">
+    <div class="drop-down">
+        <select>
+            <option value="" disabled selected>Select a base currency</option>
+            <option value="AUD">AUD</option>
+            <option value="BGN">BGN</option>
+            <option value="BRL">BRL</option>
+            <option value="CAD">CAD</option>
+            <option value="CHF">CHF</option>
+            <option value="CNY">CNY</option>
+            <option value="CZK">CZK</option>
+            <option value="DKK">DKK</option>
+            <option value="EUR">EUR</option>
+            <option value="GBP">GBP</option>
+            <option value="HKD">HKD</option>
+            <option value="HRK">HRK</option>
+            <option value="HUF">HUF</option>
+            <option value="IDR">IDR</option>
+            <option value="ILS">ILS</option>
+            <option value="INR">INR</option>
+            <option value="ISK">ISK</option>
+            <option value="JPY">JPY</option>
+            <option value="KRW">KRW</option>
+            <option value="MXN">MXN</option>
+            <option value="MYR">MYR</option>
+            <option value="NOK">NOK</option>
+            <option value="NZD">NZD</option>
+            <option value="PHP">PHP</option>
+            <option value="PLN">PLN</option>
+            <option value="RON">RON</option>
+            <option value="RUB">RUB</option>
+            <option value="SEK">SEK</option>
+            <option value="SGD">SGD</option>
+            <option value="THB">THB</option>
+            <option value="TRY">TRY</option>
+            <option value="USD">USD</option>
+            <option value="ZAR">ZAR</option>
+        </select>
+    </div>
+    <div class="drop-down">
+        <select>
+            <option value="" disabled selected>Select a target currency</option>
+            <option value="AUD">AUD</option>
+            <option value="BGN">BGN</option>
+            <option value="BRL">BRL</option>
+            <option value="CAD">CAD</option>
+            <option value="CHF">CHF</option>
+            <option value="CNY">CNY</option>
+            <option value="CZK">CZK</option>
+            <option value="DKK">DKK</option>
+            <option value="EUR">EUR</option>
+            <option value="GBP">GBP</option>
+            <option value="HKD">HKD</option>
+            <option value="HRK">HRK</option>
+            <option value="HUF">HUF</option>
+            <option value="IDR">IDR</option>
+            <option value="ILS">ILS</option>
+            <option value="INR">INR</option>
+            <option value="ISK">ISK</option>
+            <option value="JPY">JPY</option>
+            <option value="KRW">KRW</option>
+            <option value="MXN">MXN</option>
+            <option value="MYR">MYR</option>
+            <option value="NOK">NOK</option>
+            <option value="NZD">NZD</option>
+            <option value="PHP">PHP</option>
+            <option value="PLN">PLN</option>
+            <option value="RON">RON</option>
+            <option value="RUB">RUB</option>
+            <option value="SEK">SEK</option>
+            <option value="SGD">SGD</option>
+            <option value="THB">THB</option>
+            <option value="TRY">TRY</option>
+            <option value="USD">USD</option>
+            <option value="ZAR">ZAR</option>
+        </select>
+    </div>
 
     <button class="add-btn" type="button">Add Conversion Rate</button>
-</div> -->
+</div>
+
 <table class="admin-table">
     <thead>
         <tr>
@@ -89,26 +96,82 @@
             <td>USD</td>
             <td>--</td>
             <td>--</td>
-            <td><button class="remove-btn" type="button">Remove</button></td>
+            <td><button class="remove-btn" type="button">
+                    <svg aria-hidden="true" focusable="false"
+                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512">
+                        <path fill="currentColor"
+                            d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
+                            class=""></path>
+                    </svg>
+                </button></td>
         </tr>
         <tr>
             <td>USD</td>
             <td>--</td>
             <td>--</td>
-            <td><button class="remove-btn" type="button">Remove</button></td>
+            <td><button class="remove-btn" type="button">
+                    <svg aria-hidden="true" focusable="false"
+                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512">
+                        <path fill="currentColor"
+                            d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
+                            class=""></path>
+                    </svg>
+                </button></td>
         </tr>
         <tr>
             <td>USD</td>
             <td>--</td>
             <td>--</td>
-            <td><button class="remove-btn" type="button">Remove</button></td>
+            <td><button class="remove-btn" type="button">
+                    <svg aria-hidden="true" focusable="false"
+                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512">
+                        <path fill="currentColor"
+                            d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
+                            class=""></path>
+                    </svg>
+                </button></td>
         </tr>
         <tr>
             <td>USD</td>
             <td>--</td>
             <td>--</td>
-            <td><button class="remove-btn" type="button">Remove</button></td>
+            <td><button class="remove-btn" type="button">
+                    <svg aria-hidden="true" focusable="false"
+                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512">
+                        <path fill="currentColor"
+                            d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
+                            class=""></path>
+                    </svg>
+                </button></td>
         </tr>
+        <tr>
+            <td>USD</td>
+            <td>--</td>
+            <td>--</td>
+            <td><button class="remove-btn" type="button">
+                    <svg aria-hidden="true" focusable="false"
+                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512">
+                        <path fill="currentColor"
+                            d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
+                            class=""></path>
+                    </svg>
+                </button></td>
+        </tr>
+        <tr>
+            <td>USD</td>
+            <td>--</td>
+            <td>--</td>
+            <td><button class="remove-btn" type="button">
+                    <svg aria-hidden="true" focusable="false"
+                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512">
+                        <path fill="currentColor"
+                            d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
+                            class=""></path>
+                    </svg>
+                </button></td>
+        </tr>
+        
+        
     </tbody>
 </table>
 
