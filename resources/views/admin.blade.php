97dee04e244ec49b54c69fca8906d397680997a9
Stephen Toms <steve@Stephens-MacBook-Pro.local>
2019-07-30 10:44:41 -0400

    partialed out sass files better added responsive admin table

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -87,6 +87,8 @@
             <th>Base</th>
             <th>Target</th>
             <th>Rate</th>
+            <th class="complex-col">Date</th>
+            <th class="complex-col">Created</th>
             <th></th>
         </tr>
     </thead>
@@ -97,14 +99,18 @@
                 <td>{{$entry->base}}</td>
                 <td>{{$entry->target}}</td>
                 <td>{{$entry->rate}}</td>
-                <td><button class="remove-btn" type="button">
+                <td class="complex-col">{{$entry->date}}</td>
+                <td class="complex-col">{{$entry->created_at}}</td>
+                <td>
+                    <button class="remove-btn" type="button">
                         <svg aria-hidden="true" focusable="false"
                             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512">
                             <path fill="currentColor"
                                 d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
                                 class=""></path>
                         </svg>
-                    </button></td>
+                    </button>
+                </td>
             </tr>
         @endforeach
     </tbody>
--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,4 +1,3 @@
 
-    <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
