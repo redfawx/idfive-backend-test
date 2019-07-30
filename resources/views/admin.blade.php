538986eb37e7ab79da790d84ae12938e14b91f6c
Stephen Toms <steve@Stephens-MacBook-Pro.local>
2019-07-29 22:57:44 -0400

    started to create admin panel table styles and layout

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,118 @@
+@include('layout.header')
+
+<!-- <div class="select-bar">
+    <select>
+        <option value="AUD">AUD</option>
+        <option value="BGN">BGN</option>
+        <option value="BRL">BRL</option>
+        <option value="CAD">CAD</option>
+        <option value="CHF">CHF</option>
+        <option value="CNY">CNY</option>
+        <option value="CZK">CZK</option>
+        <option value="DKK">DKK</option>
+        <option value="EUR">EUR</option>
+        <option value="GBP">GBP</option>
+        <option value="HKD">HKD</option>
+        <option value="HRK">HRK</option>
+        <option value="HUF">HUF</option>
+        <option value="IDR">IDR</option>
+        <option value="ILS">ILS</option>
+        <option value="INR">INR</option>
+        <option value="ISK">ISK</option>
+        <option value="JPY">JPY</option>
+        <option value="KRW">KRW</option>
+        <option value="MXN">MXN</option>
+        <option value="MYR">MYR</option>
+        <option value="NOK">NOK</option>
+        <option value="NZD">NZD</option>
+        <option value="PHP">PHP</option>
+        <option value="PLN">PLN</option>
+        <option value="RON">RON</option>
+        <option value="RUB">RUB</option>
+        <option value="SEK">SEK</option>
+        <option value="SGD">SGD</option>
+        <option value="THB">THB</option>
+        <option value="TRY">TRY</option>
+        <option value="USD">USD</option>
+        <option value="ZAR">ZAR</option>
+    </select>
+    <select>
+        <option value="AUD">AUD</option>
+        <option value="BGN">BGN</option>
+        <option value="BRL">BRL</option>
+        <option value="CAD">CAD</option>
+        <option value="CHF">CHF</option>
+        <option value="CNY">CNY</option>
+        <option value="CZK">CZK</option>
+        <option value="DKK">DKK</option>
+        <option value="EUR">EUR</option>
+        <option value="GBP">GBP</option>
+        <option value="HKD">HKD</option>
+        <option value="HRK">HRK</option>
+        <option value="HUF">HUF</option>
+        <option value="IDR">IDR</option>
+        <option value="ILS">ILS</option>
+        <option value="INR">INR</option>
+        <option value="ISK">ISK</option>
+        <option value="JPY">JPY</option>
+        <option value="KRW">KRW</option>
+        <option value="MXN">MXN</option>
+        <option value="MYR">MYR</option>
+        <option value="NOK">NOK</option>
+        <option value="NZD">NZD</option>
+        <option value="PHP">PHP</option>
+        <option value="PLN">PLN</option>
+        <option value="RON">RON</option>
+        <option value="RUB">RUB</option>
+        <option value="SEK">SEK</option>
+        <option value="SGD">SGD</option>
+        <option value="THB">THB</option>
+        <option value="TRY">TRY</option>
+        <option value="USD">USD</option>
+        <option value="ZAR">ZAR</option>
+    </select>
+
+    <button class="add-btn" type="button">Add Conversion Rate</button>
+</div> -->
+<table class="admin-table">
+    <thead>
+        <tr>
+            <th>Base</th>
+            <th>Target</th>
+            <th>Rate</th>
+            <th></th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <tr>
+            <td>USD</td>
+            <td>--</td>
+            <td>--</td>
+            <td><button class="remove-btn" type="button">Remove</button></td>
+        </tr>
+        <tr>
+            <td>USD</td>
+            <td>--</td>
+            <td>--</td>
+            <td><button class="remove-btn" type="button">Remove</button></td>
+        </tr>
+        <tr>
+            <td>USD</td>
+            <td>--</td>
+            <td>--</td>
+            <td><button class="remove-btn" type="button">Remove</button></td>
+        </tr>
+        <tr>
+            <td>USD</td>
+            <td>--</td>
+            <td>--</td>
+            <td><button class="remove-btn" type="button">Remove</button></td>
+        </tr>
+    </tbody>
+</table>
+
+
+
+
+@include('layout.footer')
